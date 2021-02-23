284715909c577bf6f7c1aac9b316b63864853e69
shinonome128 <shinonome128@gmail.com>
2021-02-24 00:22:51 +0900

    Change 271.

--- config_dev.js
+++ config_dev.js
@@ -1,32 +1,22 @@
 var config = {"settings": {"add_css_file":false , "add_js_file":false , "custom_lang":false , "head_write_js":"" , "head_write_css":"" , "format":"table" , "messages":{"intro":"Salesforce 認定デベロッパー 練習問題"}} ,"questions":[
- {"question": "■すべてのデータを月に1回バックアップしたい、開発者はどのアプローチを使用すべき" ,"choice":["スケジュール可能なApexクラス" ,"レポートをスケジュール" ,"データエクスポートのスケジュールされたジョブ"] ,"answer":["データエクスポートのスケジュールされたジョブ"] ,"feedback_tf":["" ,""]}
-, {"question": "■キャッチできない例外タイプはどれですか？" ,"choice":["CalloutException" ,"カスタム例外" ,"NoAccessException" ,"LimitException"] ,"answer":["LimitException"] ,"feedback_tf":["" ,""]}
-, {"question": "■トリガーが起動できる回数に影響を与える3つの操作はどれ（3つ）" ,"choice":["プロセスフロー" ,"ワークフロールール" ,"メールメッセージ" ,"積み上げ集計項目"] ,"answer":["プロセスフロー" ,"ワークフロールール" ,"積み上げ集計項目"] ,"feedback_tf":["" ,""]}
-, {"question": "■数式に関連する2つの特性は何ですか？ 2つの答えを選んでください。" ,"choice":["式は自分自身を参照できる" ,"数式は実行時に計算され、データベースには保存されない" ,"数式は関連オブジェクトの値を参照できる" ,"数式使用されるフィールドは、数式なしで削除または編集できる"] ,"answer":["数式は実行時に計算され、データベースには保存されない" ,"数式は関連オブジェクトの値を参照できる"] ,"feedback_tf":["" ,""]}
-, {"question": "カスタム例外クラスについて（3つ選択）" ,"choice":["メンバー変数をもてない" ,"インターフェースの実装ができる" ,"カスタムクラスを継承できる" ,"システム定義の基本クラスを拡張する" ,"クラス名の最後にExceptionをつける"] ,"answer":["インターフェースの実装ができる" ,"システム定義の基本クラスを拡張する" ,"クラス名の最後にExceptionをつける"] ,"feedback_tf":["" ,""]}
-, {"question": "名前空間外からアクセス可能なメソッドを記述する方法（1つ選択）" ,"choice":["クラスとメソッドにglobalをつける" ,"クラスとメソッドにpublicをつける" ,"クラスにglobal、メソッドにpublicにつける" ,"クラスにpublic、メソッドにglobalをつける"] ,"answer":["クラスとメソッドにglobalをつける"] ,"feedback_tf":["" ,""]}
-, {"question": "Account acc = [SELECT Name FROM Account];の結果が複数件あった場合どうなる？（1つ選択）" ,"choice":["例外が発生する？" ,"エラーログに記録される" ,"先頭の1件がaccに格納される" ,"accがリストに変換される"] ,"answer":["例外が発生する？"] ,"feedback_tf":["" ,""]}
-, {"question": "batchableインターフェースを実装したクラスのガバナ制限について" ,"choice":["非同期だから関係ない" ,"非同期だから制限が厳しくなる" ,"with sharingをつけなかった場合の挙動" ,"システムモードで実行"] ,"answer":["システムモードで実行"] ,"feedback_tf":["" ,""]}
-, {"question": "選択リストで必須項目にする方法（1つ選択）" ,"choice":["ページレイアウトで設定" ,"デフォルト値を設定" ,"Apexトリガー"] ,"answer":["ページレイアウトで設定"] ,"feedback_tf":["" ,""]}
-, {"question": "テストカバー率の確認方法（1つ選択）" ,"choice":["TestタブのOverall Code Coverage"] ,"answer":["TestタブのOverall Code Coverage"] ,"feedback_tf":["" ,""]}
-, {"question": "Apexジョブキューの監視方法（2つ選択）" ,"choice":["Apexジョブページ" ,"Apex Flexキュー"] ,"answer":["Apexジョブページ" ,"Apex Flexキュー"] ,"feedback_tf":["" ,""]}
-, {"question": "■データ容量2GB､本番データは不要のインテグレーションテストを行いたい。次のうち要件を満たすものはどれですか？（２つ選択）" ,"choice":["Developer Sandbox" ,"DeveloperPro Sandbox" ,"Partical Copy Sandbox" ,"Full Sandbox"] ,"answer":["Partical Copy Sandbox" ,"Full Sandbox"] ,"feedback_tf":["" ,""]}
-, {"question": "■次のうち、エラーとならないものはどれですか？（２つ選択）" ,"choice":["Integer a = 3.14;" ,"Double a = 3.14;" ,"Long a = 3.14;" ,"Decimal a = 3.14;"] ,"answer":["Double a = 3.14;" ,"Decimal a = 3.14;"] ,"feedback_tf":["" ,""]}
-, {"question": "■本番組織にメタデータをリリースできるのは？（3つ選択）" ,"choice":["Sandbox変更セット" ,"Force.com IDE" ,"メタデータAPI" ,"データローダ" ,"DeveloperEdition組織変更セット"] ,"answer":["Sandbox変更セット" ,"Force.com IDE" ,"メタデータAPI"] ,"feedback_tf":["" ,""]}
-, {"question": "■取引責任者を持たない取引先名NoContactsのデータで実行した場合の結果は？<pre><br>List a = new List([select Id,Name ,(select id,name from contacts) from account where Name = 'NoContacts']);<br></pre>" ,"choice":["a[0].contacts は、null" ,"a[0].contacts は、空リスト" ,"a[0]は、null" ,"apexエラーになる"] ,"answer":["a[0]は、null"] ,"feedback_tf":["" ,""]}
-, {"question": "■<ltng:require>の機能は？（3つ選択）" ,"choice":["１回のみ読み込み" ,"並列読み込み" ,"外部javascriptの読み込み" ,"ドキュメントの読み込み"] ,"answer":["１回のみ読み込み" ,"並列読み込み" ,"外部javascriptの読み込み"] ,"feedback_tf":["" ,""]}
-, {"question": "■商談商品トリガのテストを実施で標準価格を設定したいが、組織で設定されているため作成できない。どうすればよいか？" ,"choice":["@testVisibleを使用して標準価格を読み込む" ,"@isTest(seeAllData=True)で標準価格を削除する" ,"Test.loadDataで?" ,"StandardPriceBookIdを?"] ,"answer":["Test.loadDataで?"] ,"feedback_tf":["" ,""]}
-, {"question": "■セールス、セールスマネージャーで両方が使うvisualforce画面がある。その画面でxxx__cという項目は、プロファイルの項目レベルセキュリティでセールスマネージャーだけが、参照できるように設定している。将来のメンテナンス性を考慮して開発者が取る方法はどれ？" ,"choice":["両方のプロファイルで使うvisualforce画面を1つ作成" ,"with sharingキーワードを使用したカスタムコントローラを作る" ,"それぞれのプロファイルで使うvisualforce画面を2つ作成" ,"新しいセールスマネージャーには、権限セットを割り当てる。"] ,"answer":["両方のプロファイルで使うvisualforce画面を1つ作成"] ,"feedback_tf":["" ,""]}
-, {"question": "■カスタム例外を作成するときの文法。下記構文をしっかり覚えてれば、ボーナス問題ですが、うろ覚えだとどれもあってそうに見えてくる。。" ,"choice":["public class MyException extends Exception {}"] ,"answer":["public class MyException extends Exception {}"] ,"feedback_tf":["" ,""]}
-, {"question": "キャンペーンメンバー〜で取引先、取引先責任者、商談を作成した場合、キャンペーンメンバーが新たに参照するオブジェクトは？（1つ選択）" ,"choice":["取引先" ,"取引先責任者" ,"商品2" ,"商談"] ,"answer":["取引先責任者"] ,"feedback_tf":["" ,""]}
-, {"question": "パラメータの戻り値の型ページURLに含まれるパラメータをgetParametersで取得した時の型は？" ,"choice":["Map<String, String>" ,"String[]" ,"List" ,"String"] ,"answer":["Map<String, String>"] ,"feedback_tf":["" ,""]}
-, {"question": "メール一括送信（夜間バッチで数万件商談作成後、取引先とリードに合ってるかメールを送る）、パフォーマンスを気にしている（2つ選ぶ）" ,"choice":["AppExchange" ,"Apex" ,"プロセスビルダー？" ,"ワークフロー？"] ,"answer":["AppExchange" ,"Apex"] ,"feedback_tf":["" ,""]}
-, {"question": "本番環境のApexクラスの削除方法" ,"choice":["Workbench"] ,"answer":["Workbench"] ,"feedback_tf":["" ,""]}
-, {"question": "テストの実行方法3つ選ぶ" ,"choice":["開発者コンソール" ,"[設定]" ,"Tooling API" ,"（これは不正解）メタAPI"] ,"answer":["開発者コンソール" ,"[設定]" ,"Tooling API"] ,"feedback_tf":["" ,""]}
-, {"question": "複数リストを1つだけ選んだとき" ,"choice":["最後がカンマで終わるString" ,"String" ,"リスト" ,"セット"] ,"answer":["String"] ,"feedback_tf":["" ,""]}
-, {"question": "開発環境から、本番環境と関連しない組織へのリリース方法" ,"choice":["Force.com IDE" ,"IDEで直接アップロードとか、本番に入って設定変更（関連組織に変更）とかあったけどわからんかった。。"] ,"answer":["Force.com IDE"] ,"feedback_tf":["" ,""]}
-, {"question": "Sample.cmp を作った、画面でカーソルを当てた時にcmpを呼び出すのはどのバンドルか" ,"choice":["sampleHelper.js" ,"sample.helper" ,"sampleRenderer.js" ,"sample.renderer"] ,"answer":["sampleHelper.js"] ,"feedback_tf":["" ,""]}
-, {"question": "Visualforceで使用するcssの方法(3つ選択)" ,"choice":["apex:stylesheets" ,"apex:stylesheet" ,"静的リソース" ,"インライン" ,"apex:style"] ,"answer":["apex:stylesheet" ,"静的リソース" ,"インライン"] ,"feedback_tf":["" ,""]}
-, {"question": "開発者コンソールで出力するログレベルはどこから変更するか1つ選択" ,"choice":["[設定]メニューから..." ,"Editメニューから -> ..." ,"Debugメニューから -> ..." ,"Logsメニューから -> ..."] ,"answer":["Debugメニューから -> ..."] ,"feedback_tf":["" ,""]}
-, {"question": "ヒープエラーになった、対応2つ選択" ,"choice":["変数にtransientつける" ,"使わないコレクションをnullまたは空にする" ,"SOQL for Loopにする"] ,"answer":["使わないコレクションをnullまたは空にする" ,"SOQL for Loopにする"] ,"feedback_tf":["" ,""]}
+{"question": "テスト内でガバナ制限に引っかかってるから確認する方法1つ選ぶ" ,"choice":["Test.getDmlStatementsを使う" ,"Limit、startTest、stopTest使う"] ,"answer":["Limit、startTest、stopTest使う"] ,"feedback_tf":["" ,""]}
+, {"question": "マルチテナントで未完成のパッケージをリリースする際に想定外のエラーに対して考慮するためにやっておいたほうがいいこと（みたいな質問、1つ選択）" ,"choice":["入力規則を全部設定してリリース（？）" ,"テストを75%"] ,"answer":["テストを75%"] ,"feedback_tf":["" ,""]}
+, {"question": "映画と俳優と契約の3オブジェクト、契約が映画と俳優を1つずつ持つ時の関連付けはどれにすればよいか（1つ選択）" ,"choice":["2つとも主従" ,"2つとも参照" ,"片方が参照、片方が主従（がそれぞれ）"] ,"answer":["2つとも主従"] ,"feedback_tf":["" ,""]}
+, {"question": "外部IDの意味（1つ）" ,"choice":["upsertで重複ふせぐ" ,"他の開発環境に移すときに同一のSalesforceIDを振る（？）" ,"sObjectとして認識するための値"] ,"answer":["upsertで重複ふせぐ"] ,"feedback_tf":["" ,""]}
+, {"question": "SOQLのインジェクションを防ぐ方法2つ選択,,実際のコードの中から選択する" ,"choice":["like検索→文字結合" ,"like検索　文字結合（String.escapeSingleQuotes）" ,"バインド変数" ,"バインド変数（String.escapeSingleQuotes実施した変数）"] ,"answer":["like検索　文字結合（String.escapeSingleQuotes）" ,"バインド変数"] ,"feedback_tf":["" ,""]}
+, {"question": "電話番号検索、複数オブジェクトをまたぐ方法（1つ選択）" ,"choice":["SOSLを実行" ,"ALL ROWS使ってSOQLクエリ" ,"ALL ROWS使ってSOSLクエリ"] ,"answer":["SOSLを実行"] ,"feedback_tf":["" ,""]}
+, {"question": "Trigger.oldに入ってる値は？" ,"choice":["更新対象の前のバージョンの値"] ,"answer":["更新対象の前のバージョンの値"] ,"feedback_tf":["" ,""]}
+, {"question": "色々なところから呼ばれるApexのヘルパーを作った、その中の一部のロジックのみ、トリガーでだけ実行したい（1つ選択）" ,"choice":["isExecuting使う" ,"Trigger.newMap != null で判定する"] ,"answer":["isExecuting使う"] ,"feedback_tf":["" ,""]}
+, {"question": "with sharing で作ったメソッドを、営業所の部長とその配下にのみ、取引先が見えるようにしたい、やるべきこと（1つ選択）" ,"choice":["プロファイル1つ、権限セット1つ、ロール1つ" ,"プロファイル2つ、権限セット1つ、ロール1つ" ,"プロファイル1つ、権限セット2つ、ロール1つ" ,"プロファイル1つ、権限セット1つ、ロール2つ"] ,"answer":["プロファイル1つ、権限セット1つ、ロール2つ"] ,"feedback_tf":["" ,""]}
+, {"question": "汎用的なオブジェクトを受け取るメソッドを作った、内部でどのオブジェクトか判定するには？（1つ選択）" ,"choice":["getSObjectTypeでオブジェクトのトークンを取得して判定" ,"getSObjectNameでオブジェクト名を取得して判定"] ,"answer":["getSObjectTypeでオブジェクトのトークンを取得して判定"] ,"feedback_tf":["" ,""]}
+, {"question": "リリース時のカバレッジ（2つ選択）", "choice": [ "トータルでカバー率100％", "トータルでカバー率75％", "Apexテスト75％成功", "Apexテスト100％成功" ], "answer": [ "トータルでカバー率75％", "Apexテスト75％成功" ], "feedback_tf": [ "", "" ] }
+, {"question": "単体テストで失敗するが、テストコードをコピーして開発者コンソールから匿名実行すると成功した。失敗する原因は何か？" ,"choice":["テストコードにtry/catchが入っている" ,"テストコードに@futureがある" ,"テストコードに構文エラーがある" ,"テストコードが既存のデータに依存している"] ,"answer":["テストコードが既存のデータに依存している"] ,"feedback_tf":["" ,""]}
+, {"question": "下記のクエリ（where句とか項目はうろ覚え）で例外が発生する可能性は？（2つ選ぶ）<pre><br>SELECT Name, (SELECT FirstName,LastName FROM Contacts) FROM Account<br></pre>" ,"choice":["SQLの複雑度に関する例外" ,"CPUのなんとかかんとか" ,"Accountの取得行数に関する例外" ,"Contactの取得行数に関する例外"] ,"answer":["Accountの取得行数に関する例外" ,"Contactの取得行数に関する例外"] ,"feedback_tf":["" ,""]}
+, {"question": "商談のステータスが更新されたら、取引先を更新した上で商談商品の内容を納品商品？にレコード追加したい（みたいな内容）何を使えば実現できるか？" ,"choice":["ワークフロー" ,"プロセスビルダー" ,"ワークフローとプロセスビルダー" ,"プロセスビルダーからフロー（うろ覚え）"] ,"answer":["プロセスビルダー"] ,"feedback_tf":["" ,""]}
+, {"question": "夜間に大量データをアップロードし、そのデータをメアドを元にリードとかと付き合わせてごにょごにょする処理があるが、パフォーマンスを懸念している。何で実現するべきか？" ,"choice":["Apex" ,"プロセスビルダー" ,"ワークフロー" ,"項目自動更新"] ,"answer":["Apex"] ,"feedback_tf":["" ,""]}
+, {"question": "拡張コントローラにsave()メソッドが定義されている。実行されるのはどれか？" ,"choice":["拡張コントローラのsave" ,"標準と拡張の両方" ,"標準のsave"] ,"answer":["拡張コントローラのsave"] ,"feedback_tf":["" ,""]}
+, {"question": "簡単なテキストを返すLightningコンポーネントを作成した。これを他の複数のApexから使えるようにするにはどうしたら良いか？" ,"choice":["各クラスにgetMessageText()を実装する" ,"インターフェースを継承してgetMessageText()を実装する" ,"getMessageText()を実装したクラスを継承する"] ,"answer":["getMessageText()を実装したクラスを継承する"] ,"feedback_tf":["" ,""]}
+, {"question": "子オブジェクトの数量を変更したら、その親オブジェクトで集計結果を簡単に見れるようにしたいみたいな問題。標準オブジェクトの主従か参照かを知っていて、どの方法使うかを選ばせるようなやつ（だと思う）。" ,"choice":["積み上げ集計項目" ,"数式" ,"ワークフロー"] ,"answer":["ワークフロー"] ,"feedback_tf":["" ,""]}
+, {"question": "セットを追加する時の注意点は" ,"choice":["equals メソッドと hashCode メソッドの追加" ,"@InvocableVariableアノテーションを付ける"] ,"answer":["equals メソッドと hashCode メソッドの追加"] ,"feedback_tf":["" ,""]}
+, {"question": "Apexが作成できる環境は" ,"choice":["Developer Edition" ,"Sandbox" ,"Professonal Edition" ,"Unlimited Edition"] ,"answer":["Developer Edition" ,"Sandbox"] ,"feedback_tf":["" ,""]}
 ]}
